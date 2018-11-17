5ac275cef72ca6b89e849b2d21feb6294d4e0cf9
Terrance Li <terranceli.terbear@gmail.com>
2018-11-17 12:49:48 -0800

    Added auth library

---
+++ auth.js
@@ -0,0 +1,21 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const saltRounds = 12
+
+exports.hash = password => {
+	return new Promise((res, rej) => {
+		bcrypt.hash(password, saltRounds, (err, hash) => {
+			if (err) rej('cannot hash the password')
+			res(hash)
+		})
+	})
+}
+
+exports.check = (inputPassword, storedPassword, callback) => {
+	return new Promise((resolve, rej) => {
+		bcrypt.compare(inputPassword, storedPassword, (err, res) => {
+			if (err) rej('error comparing')
+			resolve(res)
+		})
+	})
+}
