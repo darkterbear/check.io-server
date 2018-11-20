3d164e68585e3965e6d75e3bdd0c716dd5bb89c1
Terrance Li <terranceli.terbear@gmail.com>
2018-11-19 16:06:11 -0800

    Sockets.js

---
+++ sockets.js
@@ -0,0 +1,52 @@
+let io
+const { Restaurant, User, Transaction } = require('./schemas')
+
+module.exports = {
+	init: (server, sessionMiddleware) => {
+		io = require('socket.io')(server)
+
+		io.use(function(socket, next) {
+			var req = socket.handshake
+			var res = {}
+			sessionMiddleware(req, res, async function(err) {
+				if (err) {
+					console.error(err)
+					let error = new Error('Internal Error')
+					error.data = {
+						type: 'internal_error'
+					}
+					return next(error)
+				}
+
+				var id = req.session._id
+
+				if (!id || !req.session.authenticated) {
+					let error = new Error('Authentication error')
+					error.data = {
+						type: 'authentication_error'
+					}
+					return next(error)
+				}
+
+				// be able to reference this restaurant later on
+				const restaurant = await Restaurant.findOne({ _id: id }).exec()
+
+				if (!restaurant) return
+
+				socket.join(id.toString())
+
+				return next()
+			})
+		})
+	},
+	onNearby: (customer, restaurants) => {
+		restaurants.forEach(r => {
+			io.to(r._id.toString()).emit('userNearby', customer)
+		})
+	},
+	onNotNearby: (customer, restaurants) => {
+		restaurants.forEach(r => {
+			io.to(r._id.toString()).emit('userLeft', customer)
+		})
+	}
+}
